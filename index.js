b453e28cbba319d2b9e37e8f7a00152aee5601c3
karissa <krmckelv@gmail.com>
2016-06-03 16:29:37 -0700

    remove console.log for rendering

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ function Tree (widget, root, entries, onclick) {
         var el = yo`<div id="data-viewport"></div>`
         data.render(entry, el, function (err, elem) {
           if (err) throw err
-          console.log('done')
           yo.update(widget, elem)
         })
       }
