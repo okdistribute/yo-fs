86158606943d605b3585246acc803f0f0fcf173d
karissa <krmckelv@gmail.com>
2017-04-14 18:24:33 -0500

    allow entry.size or entry.length

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ Tree.prototype.render = function (root, entries, onclick) {
     return yo`<tr class='entry ${entry.type}' onclick=${click}>
       <td class="name">${path.basename(entry.name)}</td>
       <td class="modified">${entry.mtime ? relative(entry.mtime) : ''}</td>
-      <td class="size">${pretty(entry.length)}</td>
+      <td class="size">${pretty(entry.size || entry.length)}</td>
     </tr>`
   }
 }
